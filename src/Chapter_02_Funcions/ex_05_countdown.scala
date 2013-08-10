f87650f90626c623dd7f5b70ba3c363e250f5e65
Diego Zambelli Sessona <dzsessona@gilt.com>
2013-08-10 19:54:37 +0100

    chapter2

---
+++ src/Chapter_02_Funcions/ex_05_countdown.scala
@@ -0,0 +1,14 @@
+package Chapter_02_Funcions
+
+object Main extends App{
+
+  def signum(n:Int): Int = n match{
+    case 0 => 0
+    case x if x<0 => -1
+    case _ => 1
+  }
+
+  println("signum(0): " +signum(0))
+  println("signum(-45): " +signum(-45))
+  println("signum(789): " +signum(789))
+}
