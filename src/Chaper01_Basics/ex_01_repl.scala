5965545d56d849ebdc294fc3166e675189fe9302
Diego Zambelli Sessona <dzsessona@gilt.com>
2013-08-10 03:33:33 +0100

    first exercise committed

---
+++ src/Chaper01_Basics/ex_01_repl.scala
@@ -0,0 +1,6 @@
+package Chaper01_Basics
+
+object Main extends App{
+  println("It prints all the methods available in the Int class. In my repl I cannot just type 3, have to declare a variable val s = 3, then go s. + TAB:\n")
+  println("%              &              *              +              -              /              >              >=             >>             >>>            ^              asInstanceOf   isInstanceOf\ntoByte         toChar         toDouble       toFloat        toInt          toLong         toShort        toString       unary_+        unary_-        unary_~        |")
+}
