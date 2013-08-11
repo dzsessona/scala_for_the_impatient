95effd226803751b451b2d27f52ad51d08ffcefb
Diego Zambelli Sessona <dzsessona@gilt.com>
2013-08-11 19:50:57 +0100

    chapter 1

--- src/Chapter_01_Basics/ex_01_repl.scala
+++ src/Chapter_01_Basics/ex_01_repl.scala
@@ -1,6 +1,6 @@
 package Chapter_01_Basics.ex1
 
 object Main extends App{
-  println("It prints all the methods available in the Int class. In my repl I cannot just type 3, have to declare a variable val s = 3, then go s. + TAB:\n")
+  println(" It prints all the methods available in the Int class. In my repl I cannot just type 3, have to declare a variable val s = 3, then go s. + TAB:\n")
   println("%              &              *              +              -              /              >              >=             >>             >>>            ^              asInstanceOf   isInstanceOf\ntoByte         toChar         toDouble       toFloat        toInt          toLong         toShort        toString       unary_+        unary_-        unary_~        |")
 }
