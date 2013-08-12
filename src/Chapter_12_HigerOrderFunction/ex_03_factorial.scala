03b64ae7bfa822417e63625f4d59a018f4ca76ca
Diego Zambelli Sessona <dzsessona@gilt.com>
2013-08-12 14:32:06 +0100

    chapter 12

---
+++ src/Chapter_12_HigerOrderFunction/ex_03_factorial.scala
@@ -0,0 +1,11 @@
+package Chapter_12_HigerOrderFunction.ex3
+
+object Main extends App{
+
+  def factorial(f:Int):Int ={
+    (f to 1 by(-1)).reduceLeft(_ * _)
+  }
+  println("factorial of 2: " + factorial(2) )
+  println("factorial of 5: " + factorial(5) )
+  println("factorial of 10: " + factorial(10) )
+}
---
+++ src/Chapter_12_HigerOrderFunction/ex_04_avoidfoldleft.scala
@@ -0,0 +1,14 @@
+package Chapter_12_HigerOrderFunction.ex4
+
+object Main extends App{
+
+  def factorial(f:Int):Int = f match {
+    case x if(x > 1) => (f to 1 by(-1)).reduceLeft(_ * _)
+    case _ =>  (f to 1 by(-1)).foldLeft(1)(_ * _)
+  }
+  println("factorial of 1: " + factorial(1) )
+  println("factorial of 0: " + factorial(0) )
+  println("factorial of 2: " + factorial(2) )
+  println("factorial of 5: " + factorial(5) )
+  println("factorial of 10: " + factorial(10) )
+}
